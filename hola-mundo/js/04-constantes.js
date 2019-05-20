161c9edd3770b77bf5dc7d6cd3456f39564dd594
benjamin.nilo@gmail.com <benjamin.nilo@gmail.com>
2019-05-20 18:41:19 -0400

    actualización

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,11 @@
+'use strict'
+//Constantes
+//Como una variable, pero su valor no puede cambiar
+
+var web = "http://www.google.com";
+const ip = "172.217.192.99";
+
+web = "http://www.google.es";
+//ip = "172.217.192.22"; // no se puede cambiar una variable constante
+
+console.log(web,ip);
---
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,15 @@
+'use strict'
+//Operadores
+var numero1 = 4;
+var numero2 = 4;
+var operacion = numero1*numero2;
+
+alert("El resultado de la operación es: "+operacion);
+
+//Tipos de datos 
+var numero_entero = 22;
+var cadena_texto = "Cadena de texto";
+var verdadero_or_false=true;
+var numero_falso="33.5";
+
+console.log(parseFloat(numero_falso)+22);
