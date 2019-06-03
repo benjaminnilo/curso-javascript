8d8baa19dcc48826ad05624757739aa1e248233e
benjamin.nilo@gmail.com <benjamin.nilo@gmail.com>
2019-06-02 22:06:53 -0400

    parametros y funciones anonimas

--- hola-mundo/js/19-ejercicio-9.js
+++ hola-mundo/js/19-ejercicio-9.js
@@ -5,29 +5,40 @@
 
 function porConsola(numero1,numero2){
 
-
-	console.log("***********console**************");
+	console.log("*****console*******");
 	console.log("Suma: "+ (numero1+numero2));
 	console.log("Resta: "+ (numero1-numero2));
 	console.log("Multiplicación: "+ (numero1*numero2));
 	console.log("División: "+ (numero1/numero2));
-	console.log("**************************************");
+	console.log("********");
 }
 
-function porPantalla(numero1, numero2, mostrar = false){
+function porPantalla(numero1, numero2){
 
-	document.write("***********Page**************"+"<br/>");
+	document.write("*****Page******"+"<br/>");
 
 	document.write("Suma: "+ (numero1+numero2)+"<br/>");
 	document.write("Resta: "+ (numero1-numero2)+"<br/>");
 	document.write("Multiplicación: "+ (numero1*numero2)+"<br/>");
 	document.write("División: "+ (numero1/numero2)+"<br/>");
 
-	document.write("**************************************");
+	document.write("********");
 }
 
+function calculadora(numero1,numero2,mostrar = false){
 
-calculadora(1,2);
+	if (mostrar==false) {
+		porConsola(numero1,numero2);
+	}else{
+		porPantalla(numero1,numero2);
+	}
+	return true;
+}
+
+calculadora(5,4,true);
+calculadora(2,5);
+calculadora(54,4,true);
+calculadora(6,4,true);
 /*
 for (var i = 1; i < 10; i++) {	
 	console.log(i);
---
+++ hola-mundo/js/21-funciones-anonimas.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//Funciones anonimas
+//Es una función que no tiene nombre
+
+var pelicula = function(nombre){
+	return "La pelicula es: " +nombre;
+}
